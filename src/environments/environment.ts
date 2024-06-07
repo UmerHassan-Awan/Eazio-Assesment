6a9eedd031f0a62ad18c5151094fe340368e92f0
Umer Hassan <umerhassan2013@gmail.com>
2024-06-07 17:36:42 +0500

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
     production: false,
-    ApiURL: 'https://develop.digicom-api.nonprod.spherewms.com/', // This is proxied on local dev to avoid CORS
+    ApiURL: '', // This is proxied on local dev to avoid CORS
   };
